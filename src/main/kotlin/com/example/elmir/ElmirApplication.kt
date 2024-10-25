4ca9533cf8dde9dc1f6558eb9227e41a0663a6e9
elmirshidev <rzazadeelmir19@gmail.com>
2024-10-25 09:09:46 +0400

    merged all

---
+++ src/main/kotlin/com/example/elmir/ElmirApplication.kt
@@ -0,0 +1,11 @@
+package com.example.elmir
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ElmirApplication
+
+fun main(args: Array<String>) {
+	runApplication<ElmirApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/elmir/ElmirApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.elmir
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ElmirApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
